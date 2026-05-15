dea010438a7c193492ca8d9b9daa1e712c4a64c2
Juan Yepes <juan.yepes@udla.edu.ec>
2026-05-15 15:18:57 -0500

    cambiar x

--- ejercicio3.c
+++ ejercicio3.c
@@ -4,10 +4,12 @@ int main(int argc, char *argv[]) {
 
     char vocales[] = {'a','e','i','o','u'};
 
+    vocales[3] = 'x';
+
     for (int i = 0; i < 5; i++)
     {
         printf("%c ", vocales[i]);
     }
 
     return 0;
-}
+} 
