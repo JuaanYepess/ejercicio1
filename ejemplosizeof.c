fac5301c6ece36b5ee0040856385bb56d73c90e9
Juan Yepes <juan.yepes@udla.edu.ec>
2026-05-15 14:47:46 -0500

    segundo commit

--- ejemplosizeof.c
+++ ejemplosizeof.c
@@ -5,7 +5,13 @@ int main(int argc, char const *argv[])
 
     int i = sizeof(int);
 
-    printf("%d byte(s)", i);
+    printf("%d byte(s)\n", i);
+
+    int arreglo[] = {3,6,9,12,15,18};
+
+    int j = sizeof arreglo;
+
+    printf("%d byte(s)", j);
 
     return 0;
 }
