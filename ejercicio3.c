2c660fd42854ed58e7cf4ea02907436d2e0319cc
Juan Yepes <juan.yepes@udla.edu.ec>
2026-05-15 15:12:05 -0500

    vocales

---
+++ ejercicio3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    char vocales[] = {'a','e','i','o','u'};
+
+    char *ptr;
+
+    ptr = vocales;
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%c ", *(ptr+i));
+    }
+
+    return 0;
+}
