220477f1d52328e9f8e7e047c172766ac9ad2047
Juan Yepes <juan.yepes@udla.edu.ec>
2026-05-15 14:28:55 -0500

    Uso de punteros para recorrer arreglos

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
     int arreglo[] = {3,6,9,12,15,18};
 
     int *ptr;
 
-    ptr = arreglo;
-
-    printf("%p\n", ptr);
+    ptr = &arreglo[0];
 
     for (int i = 0; i < 6; i++) {
 
-        printf("%d ", arreglo[i]);
+        printf("arreglo[%d] = %d ", i, arreglo[i]);
+
+        printf("%d\n", *(ptr + i));
 
     }
 
